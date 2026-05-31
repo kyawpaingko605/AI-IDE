22bb9dbb92bbea73f505b1c8c864cbe4e0a09715
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-05-31 12:25:26 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
 
-    // 🎨 Jetpack Compose Stable Versions (BOM မသုံးဘဲ အသေကြေညာပေးခြင်းဖြင့် ရှာမတွေ့သည့် ပြဿနာကို ကျော်လွှားခြင်း)
+    // 🎨 Jetpack Compose Stable Versions
     implementation("androidx.compose.ui:ui:1.6.1")
     implementation("androidx.compose.ui:ui-graphics:1.6.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
